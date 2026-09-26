7b9cba51e799479a93c44a2c048f567017bcdefb
julius1523 <juliusdolana13@gmail.com>
2026-09-26 22:18:53 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-env', fn() => response()->json([
+    'env_aws_url' => env('AWS_URL'),
+    'config_public_url' => config('filesystems.disks.public.url'),
+]));
+
 Route::middleware('prevent_back')
     ->get('/{any}', fn() => response()->view('app'))
     ->where('any', '.*');
